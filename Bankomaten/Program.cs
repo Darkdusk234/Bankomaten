4776b374ea1dc2404780fa88688002de1d9fd635
darkdusk234 <johan.svensson.01@hotmail.com>
2024-09-25 10:24:17 +0200

    Added Write Method to write text in rainbow pattern

--- Bankomaten/Program.cs
+++ Bankomaten/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+
+        /// <summary>
+        /// Prints out a string of text in a rainbowlike pattern
+        /// </summary>
+        /// <param name="text"></param>
+        static void Write(string text)
+        {
+            ConsoleColor[] pallette = new ConsoleColor[] { ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Green, ConsoleColor.Cyan, ConsoleColor.Magenta };
+            int color = 0;
+
+            foreach (char c in text)
+            {
+                Console.ForegroundColor = pallette[color];
+                Console.Write(c);
+
+                if (char.IsWhiteSpace(c) == false)
+                {
+                    color++;
+                }
+                if (color > 4)
+                {
+                    color = 0;
+                }
+            }
+            Console.ResetColor();
+            Console.Write("\n");
         }
     }
 }
